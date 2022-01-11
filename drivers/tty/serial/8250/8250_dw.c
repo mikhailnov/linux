e32b0e14f392f6abcda2bd6030e87d851bfe2bef
Alexey Sheplyakov <asheplyakov@basealt.ru>
2022-01-11 15:49:19 +0400

    serial: 8250_dw: verify clock rate in dw8250_set_termios
    Refuse to change the clock rate if clk_round_rate() returns
    a rate which is way too off (i.e. by more than 1/16 from the one
    necessary for a given baud rate). In particular this happens if
    the requested rate is below the minimum supported by the clock.
    
    Fixes the UART console on Baikal-M SoC. Without this patch the
    console gets garbled immediately after loading the driver.
    dw8250_set_termios tries to configure the baud rate (115200),
    and calls clk_round_rate to figure out the supported rate closest
    to 1843200 Hz (which is 115200 * 16). However the (SoC-specific)
    clock driver returns 4705882 Hz. This frequency is way too off,
    hence after setting it the console gets garbled.
    
    Signed-off-by: Alexey Sheplyakov <asheplyakov@basealt.ru>
    Signed-off-by: Vadim V. Vlasov <vadim.vlasov@elpitech.ru>
    
    Cc: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Cc: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Cc: Serge Semin <Sergey.Semin@baikalelectronics.ru>

--- drivers/tty/serial/8250/8250_dw.c
+++ drivers/tty/serial/8250/8250_dw.c
@@ -332,14 +332,15 @@ dw8250_do_pm(struct uart_port *port, unsigned int state, unsigned int old)
 static void dw8250_set_termios(struct uart_port *p, struct ktermios *termios,
 			       struct ktermios *old)
 {
-	unsigned long newrate = tty_termios_baud_rate(termios) * 16;
+	unsigned long baud = tty_termios_baud_rate(termios);
+	unsigned long newrate = baud * 16;
 	struct dw8250_data *d = to_dw8250_data(p->private_data);
 	long rate;
 	int ret;
 
 	clk_disable_unprepare(d->clk);
 	rate = clk_round_rate(d->clk, newrate);
-	if (rate > 0) {
+	if (rate > 0 && rate >= baud * 15 && rate <= baud * 17) {
 		/*
 		 * Premilinary set the uartclk to the new clock rate so the
 		 * clock update event handler caused by the clk_set_rate()
